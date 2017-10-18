51c60f6da8384e8c91eb3f9bcc47d0c802e1d228
woody0907 <ningdi0907@gmail.com>
2017-10-18 11:02:32 +0800

    use json.dumps

--- lwmi.py
+++ lwmi.py
@@ -1,10 +1,33 @@
+# -*- coding: utf-8 -*-
 import wmi_client_wrapper as wmi
+import sys
+import json
+
+# wmic = wmi.WmiClientWrapper(
+#     username="administrator",
+#     password="szsyl123",
+#     host="191.168.3.158"
+# )
+
+script, cip, cuser, cpass = sys.argv
 
 wmic = wmi.WmiClientWrapper(
-    username="administrator",
-    password="szsyl123",
-    host="191.168.3.158"
+    username=cuser,
+    password=cpass,
+    host=cip
 )
 
-output = wmic.query("SELECT * FROM Win32_Processor")
-print(output)
+# 通过wmi获取不同的指标，获取方法类似
+Win32_Processor = wmic.query("SELECT * FROM Win32_Processor")
+Win32_Battery = wmic.query("SELECT * FROM Win32_Battery")
+Win32_PortableBattery = wmic.query("SELECT * FROM Win32_PortableBattery")
+
+
+outjson = {}
+# 放数到输出对象中
+outjson["Win32_Processor"] = Win32_Processor
+outjson["Win32_Battery"] = Win32_Battery
+outjson["Win32_PortableBattery"] = Win32_PortableBattery
+# 将结果转换成Json并输出
+pj = json.dumps(outjson)
+print(pj)
